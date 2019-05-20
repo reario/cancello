45a0a44c9cfb9627d12110b7f13497b0786692ed
Vittorio <reario@inwind.it>
2019-05-20 07:32:11 +0200

    la connessione al plc ora è interna alla funzione operate

--- cancello.c
+++ cancello.c
@@ -13,7 +13,6 @@
 #include <sys/time.h>
 #include <time.h>
 #include <modbus.h>
-//#include "gh.h"
 
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 
@@ -31,11 +30,9 @@
 #define LOCK_FILE       "/home/reario/cancello/cancello.lock"
 #define LOG_FILE        "/home/reario/cancello/cancello.log"
 
-modbus_t *mb_plc;
+//modbus_t *mb_plc;
 modbus_t *mb_zbrn1;
 uint8_t coilmap[] = {APERTURA_PARZIALE,APERTURA_TOTALE};
-
-
 
 int ts(char * tst, char * fmt)
 {
@@ -73,44 +70,7 @@ void logvalue(char *filename, char *message)
   fclose(logfile);
 }
 
-int pulsante(modbus_t *m, uint8_t bobina, uint8_t valore) {
-  char errmsg[100];
-  
-  if ( modbus_write_bit(m,bobina,valore) != 1 ) { // valore = TRUE se da accendere, FALSE se da spegnere
-    // The modbus_write_bit() function shall return 1 if successful. 
-    // Otherwise it shall return -1 and set errno.
-    sprintf(errmsg,"ERRORE DI SCRITTURA:PULSANTE ON %s\n",modbus_strerror(errno));
-    logvalue(LOG_FILE,errmsg);
-    return -1;
-  }
-  return 1; 
-}
-
-int pulsante_old(modbus_t *m,int bobina) {
-
-  char errmsg[100];
-
-  if ( modbus_write_bit(m,bobina,TRUE) != 1 ) {
-    // The modbus_write_bit() function shall return 1 if successful. 
-    // Otherwise it shall return -1 and set errno.
-    sprintf(errmsg,"ERRORE DI SCRITTURA:PULSANTE ON %s\n",modbus_strerror(errno));
-    logvalue(LOG_FILE,errmsg);
-    return -1;
-  }
-
-  sleep(1);
-
-  if ( modbus_write_bit(m,bobina,FALSE) != 1 ) {
-    sprintf(errmsg,"ERRORE DI SCRITTURA:PULSANTE OFF %s\n",modbus_strerror(errno));
-    logvalue(LOG_FILE,errmsg);
-    return -1;
-  }
-
-  return 0;
-}
-
 void rotate() {
-
   char t[20];
   char newname[40];
   char logmsg[100];
@@ -125,18 +85,17 @@ void rotate() {
   close(fd);
 }
 
-void myCleanExit(char * from) {
+void myCleanExit(char *from) {
   logvalue(LOG_FILE,from);
   unlink(LOCK_FILE);
   logvalue(LOG_FILE,"\tChiudo la connessione con PLC e ZBRN1\n");
-  modbus_close(mb_plc);
-  modbus_free(mb_plc);
-  logvalue(LOG_FILE,"\tLibero la memoria dalle strutture create\n");
+  //modbus_close(mb_plc);
   modbus_close(mb_zbrn1);
+  logvalue(LOG_FILE,"\tLibero la memoria dalle strutture create\n");
+  //modbus_free(mb_plc);
   modbus_free(mb_zbrn1);
   logvalue(LOG_FILE,"Fine.\n");
   logvalue(LOG_FILE,"****************** END **********************\n");
-  
 }
 
 void signal_handler(int sig)
@@ -193,10 +152,19 @@ void daemonize()
   logvalue(LOG_FILE,"****************** START **********************\n");
 }
 
-uint8_t operate(modbus_t *m,uint8_t current,uint8_t onoff)
+//uint8_t operate(modbus_t *m,uint8_t current,uint8_t onoff)
+uint8_t operate(uint8_t current,uint8_t onoff)
 {
+  modbus_t *mb_plc;
   char msg[100];
   uint8_t bobina;
+  mb_plc = modbus_new_tcp(PLC_IP,PORT);
+  if ( (modbus_connect(mb_plc) == -1 )) {
+      sprintf(msg,"ERRORE non riesco a connettermi con il PLC %s\n",modbus_strerror(errno));
+      //logvalue(LOG_FILE,errmsg);
+      
+      return -1;
+  }
   sprintf(msg,"bit [%u] transizione %s",current,onoff ? "0->1\n":"1->0\n");
   logvalue(LOG_FILE,msg);
   bobina=coilmap[current];
@@ -204,15 +172,18 @@ uint8_t operate(modbus_t *m,uint8_t current,uint8_t onoff)
   // sprintf(msg,"modbus_write_bit(m,%u,%s);\n",bobina,onoff?"ON":"OFF");
   logvalue(LOG_FILE,msg);
   if // (0)
-    (modbus_write_bit(m,bobina,onoff) != 1 )
+    (modbus_write_bit(mb_plc,bobina,onoff) != 1 )
     {
       sprintf(msg,"ERRORE DI SCRITTURA:PULSANTE %s %s\n",onoff ? "0->1\n":"1->0\n",modbus_strerror(errno));
       logvalue(LOG_FILE,msg);
+      modbus_close(mb_plc);
+      modbus_free(mb_plc);
       return -1;
-    }  
+    }
+  modbus_close(mb_plc);
+  modbus_free(mb_plc);
   return 1;
 }
-
 
 int main (int argc, char ** argv) {
 
@@ -222,7 +193,7 @@ int main (int argc, char ** argv) {
   
   daemonize();
   
-  mb_plc = modbus_new_tcp(PLC_IP,PORT);
+  //mb_plc = modbus_new_tcp(PLC_IP,PORT);
   mb_zbrn1 = modbus_new_tcp(ZBRN1_IP,PORT);
 
   uint16_t oldval = 0;
@@ -232,16 +203,17 @@ int main (int argc, char ** argv) {
   uint32_t response_timeout_sec = 2;
   uint32_t response_timeout_usec = 0;
 
-  modbus_set_response_timeout(mb_plc,  response_timeout_sec, response_timeout_usec); // 4 seconds 0 usec
+  //modbus_set_response_timeout(mb_plc,  response_timeout_sec, response_timeout_usec); // 4 seconds 0 usec
   modbus_set_response_timeout(mb_zbrn1,response_timeout_sec, response_timeout_usec); // 4 seconds 0 usec
   modbus_set_slave(mb_zbrn1,248);
   
-  if ( (modbus_connect(mb_plc) == -1 )) {
-  sprintf(errmsg,"ERRORE non riesco a connettermi con il PLC %s\n",modbus_strerror(errno));
-  //logvalue(LOG_FILE,errmsg);
-  myCleanExit(errmsg);
-  exit(EXIT_FAILURE);
-}
+ /*  if ( (modbus_connect(mb_plc) == -1 )) { */
+/*   sprintf(errmsg,"ERRORE non riesco a connettermi con il PLC %s\n",modbus_strerror(errno)); */
+/*   //logvalue(LOG_FILE,errmsg); */
+/*   myCleanExit(errmsg); */
+/*   exit(EXIT_FAILURE); */
+/* } */
+  
   
   if ( (modbus_connect(mb_zbrn1) == -1)) {
   sprintf(errmsg,"ERRORE non riesco a connettermi con ZBRN1. Premature exit [%s]\n",modbus_strerror(errno));
@@ -253,22 +225,19 @@ int main (int argc, char ** argv) {
   oldval = newval;
   while (1) {
     
-    if ( modbus_read_registers(mb_zbrn1, 0, 1, zbrn1_reg) < 0 ) {    // leggo lo stato degli ingressi collegati al wireless button
-      
+    if ( modbus_read_registers(mb_zbrn1, 0, 1, zbrn1_reg) < 0 ) {
+      // leggo lo stato degli ingressi collegati al wireless button
       numerr++;
       sprintf(errmsg,"ERRORE Lettura Registro ZBRN1 per Cancello [%s]. Num err [%i]\n",modbus_strerror(errno),numerr);
       logvalue(LOG_FILE,errmsg);
-    
       modbus_close(mb_zbrn1);      
       modbus_free( mb_zbrn1);
       mb_zbrn1 = modbus_new_tcp(ZBRN1_IP,PORT);
-      response_timeout_sec = 4;
-      response_timeout_usec = 0;
       modbus_set_response_timeout(mb_zbrn1, response_timeout_sec, response_timeout_usec); 
       modbus_set_slave(mb_zbrn1,248);
       
       if (modbus_connect(mb_zbrn1)==-1) {
-	sprintf(errmsg,"\tERRORE riconnessione ZBRN1 [%s]. Num err [%i]\n",modbus_strerror(errno),numerr);
+	sprintf(errmsg,"\tERRORE Riconnessione ZBRN1 [%s]. Num err [%i]\n",modbus_strerror(errno),numerr);
 	logvalue(LOG_FILE,errmsg);
       }
       
@@ -280,10 +249,6 @@ int main (int argc, char ** argv) {
 	exit(EXIT_FAILURE);
       }
     } else {
-
-#define TOPOLINO
-      
-#ifdef TOPOLINO
       newval=zbrn1_reg[0]; // registro 1 dove sono messi i primi 16 pulsanti (bit da 0 a 15)
       uint16_t diff = newval ^ oldval;
       if (diff) {
@@ -292,98 +257,15 @@ int main (int argc, char ** argv) {
 	  if (CHECK_BIT(diff,curr)) {
 	    // vedo se il bit curr di oldval è a 0 o a 1
 	    // se era a 0 in newval è passato a 1 e viceversa
-	    operate(mb_plc, curr, CHECK_BIT(oldval,curr)?FALSE:TRUE);
+	    operate(curr, CHECK_BIT(oldval,curr)?FALSE:TRUE);
+	    //operate(mb_plc, curr, CHECK_BIT(oldval,curr)?FALSE:TRUE);
 	  }
 	}	
       }
-#endif
-      
-#ifdef PAPERINO
-      newval=zbrn1_reg[0]; // registro 1 dove sono messi i primi 16 pulsanti (bit da 0 a 15)
-      if (oldval != newval) {
-	uint8_t curr;
-	for (curr = 0; curr<2; curr++) {
-	  if (!CHECK_BIT(oldval,curr) && CHECK_BIT(newval,curr)) {
-	    operate(mb_plc,curr,TRUE);
-	  }
-	  if (CHECK_BIT(oldval,curr) && !CHECK_BIT(newval,curr)) {
-	    operate(mb_plc,curr,FALSE);
-	  }	  
-	}
-      }
-#endif
-#ifdef PLUTO
-      newval=zbrn1_reg[0]; // registro 1 dove sono messi i primi 16 pulsanti (bit da 0 a 15)
-      if (oldval != newval) {
-	uint8_t curr;
-	for (curr = 0; curr<2; curr++) {
-	  if (!CHECK_BIT(oldval,curr) && CHECK_BIT(newval,curr)) {
-	    // 0->1
-	    switch ( curr ) {
-	    case 0: {
-	      // bit posizione 0
-	      sprintf(errmsg,"*bit 0 transizione 0->1 %u --> %u [%u]\n",oldval,newval,curr);
-	      logvalue(LOG_FILE,errmsg);
-	      // connect
-	      // modbus_t *mb_plc = modbus_new_tcp(PLC_IP,PORT);
-	      // if (modbus_connect(mb_plc) != -1) { 
-	      if (pulsante(mb_plc,APERTURA_PARZIALE,TRUE)<0) {
-		logvalue(LOG_FILE,"\tproblemi con pulsante\n");
-	      } 
-	      // close
-	      // mobus_close(mb_plc);b
-	      // } else {
-	      //   sprintf(errmsg,"ERRORE non riesco a connettermi con il PLC %s\n",modbus_strerror(errno));
-	      //   logvalue(LOG_FILE,errmsg);
-	      // }
-	      // mbus_free(mb_plc);
-	      break;
-	    }
-	    case 1: {
-	      // bit posizione 1
-	      sprintf(errmsg,"bit 1 transizione 0->1 %u --> %u [%u]\n",oldval,newval,curr);
-	      logvalue(LOG_FILE,errmsg);
-	      if (pulsante(mb_plc,APERTURA_TOTALE,TRUE)<0) {
-		logvalue(LOG_FILE,"\tproblemi con pulsante\n");
-	      }
-	      break;
-	    }
-	    } 
-	  }
-	  if (CHECK_BIT(oldval,curr) && !CHECK_BIT(newval,curr)) {
-	    // 1->0
-	    switch ( curr ) {
-	    case 0: {
-	      // bit posizione 0
-	      sprintf(errmsg,"*bit 0 transizione 1->0 %u --> %u\n",oldval,newval);
-	      logvalue(LOG_FILE,errmsg);
-	      if (pulsante(mb_plc,APERTURA_PARZIALE,FALSE)<0) {
-		logvalue(LOG_FILE,"\tproblemi con pulsante\n");
-	      }
-	      break;
-	    }
-	    case 1: {
-	      // bit posizione 1
-	      sprintf(errmsg,"bit 1 transizione 1->0 %u --> %u\n",oldval,newval);
-	      logvalue(LOG_FILE,errmsg);
-	      if (pulsante(mb_plc,APERTURA_TOTALE,FALSE)<0) {
-		logvalue(LOG_FILE,"\tproblemi con pulsante\n");
-	      }
-	      break;
-	    }
-	    } // switch
-	  } // if (CHECK_BIT()......
-	} // for ( curr ) { .......
-      }
-#endif
     numerr=0;
     oldval=newval;                 
   } // else
-    usleep(100000);
+    usleep(200000); // 1/10 sec
   } // while
-  
-  return 0;
-  
+  return 0; 
 }
-
-
